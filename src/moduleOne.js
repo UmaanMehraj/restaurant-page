fb6aaa2555110456aac7918d3f79431b2bd9c2ec
UmaanMehraj <umaanmehraj5@gmail.com>
2026-03-07 01:22:33 +0530

    add: img, styles

--- src/moduleOne.js
+++ src/moduleOne.js
@@ -1,20 +1,23 @@
+import image from "./img.jpg"
+
 const fun = ()=>{
     const parentDiv = document.createElement('div')
     parentDiv.setAttribute('id', 'parentDiv')
 
     const headLine = document.createElement('div')
     headLine.setAttribute('id', 'headLine')
-    headLine.innerText = "Mell's Kitchen"
+    headLine.innerText = "Welcome to Odin's"
 
-    const TwoLine = document.createElement('div')
-    TwoLine.setAttribute('id', 'headLine')
-    TwoLine.innerText = "Mello's Kitchen"
+    const imgDiv = document.createElement('div')
+    imgDiv.setAttribute('id', 'img')
+    const img = document.createElement('img')
+    img.src = image
 
+    imgDiv.append(img)
 
-    parentDiv.append(headLine, TwoLine)
+    parentDiv.append(headLine, imgDiv)
 
     return parentDiv
 }
 
 export {fun}
-
