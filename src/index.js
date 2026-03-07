8c8a76ad52a4b218cd37ba76e8cefa14cf9b5cd1
UmaanMehraj <umaanmehraj5@gmail.com>
2026-03-07 13:50:33 +0530

    add: toggle functionality

--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ import { menu } from "./menu.js"
 import { about} from "./aboutPage.js"
 
 const contentDiv = document.querySelector('#content')
-const subDivOne = home()
-// contentDiv.append(subDivOne)
 
-const subDivTwo = menu()
-// contentDiv.append(subDivTwo)
+const homePage = home()
+const menuPage = menu()
+const aboutPage = about()
 
-const subdivThree = about()
-contentDiv.append(subdivThree)
+contentDiv.append(homePage)
+
+const homeBtn = document.querySelector('#homeBtn')
+homeBtn.addEventListener('click', ()=>{
+    contentDiv.innerHTML = ''
+    contentDiv.append(homePage)
+})
+
+const menuBtn = document.querySelector('#menuBtn')
+menuBtn.addEventListener('click', ()=>{
+    contentDiv.innerHTML = ''
+    contentDiv.append(menuPage)
+})
+
+const abtBtn = document.querySelector('#abtBtn')
+abtBtn.addEventListener('click', ()=>{
+    contentDiv.innerHTML = ''
+    contentDiv.append(aboutPage)
+})
